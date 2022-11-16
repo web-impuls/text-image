9f01e39b63a3f6a689b47c921e2ffd7b2224d5d0
vebbilder <66235658+vebbilder@users.noreply.github.com>
2022-11-16 15:41:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const { Client, MessageActionRow, MessageButton } = require('discord.js');
 const config = require('./config.json');
-const { keepAlive, getImage, TextImageRedirect } = require('./server');
+const { keepAlive, textVoice, getImage, textClear } = require('./server');
 config.cfg.intents = new Discord.Intents(config.cfg.intents);
 const client = new Discord.Client(config.cfg);
 
