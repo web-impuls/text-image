7f1d4de5b22f0531c0c0a5e11ec3ac02ae27e077
vebbilder <66235658+vebbilder@users.noreply.github.com>
2022-11-19 18:22:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,46 +4,47 @@ const config = require('./config.json');
 const { keepAlive, TextImageRedirect } = require('./server');
 config.cfg.intents = new Discord.Intents(config.cfg.intents);
 const client = new Discord.Client(config.cfg);
+const dotenv = require('dotenv').config();
 
-client.login(config.token);
+client.login(process.env.ACCESS_TOKEN);
 
-    client.on('ready', async(r) => {
-        console.log('сообщение из сервера');
-    });
-    client.on('messageCreate', async(message) => {
-        // блокируем пустое сообщение
-        if (!message.content.replace(/<(.|\n)*?>/g, '').trim()) {
-            return false;
-        }
-        //
-        else if (message.content.includes("@here") || message.content.includes("@everyone")) return false;
+client.on('ready', async(r) => {
+    console.log('сообщение из сервера');
+});
+client.on('messageCreate', async(message) => {
+    // блокируем пустое сообщение
+    if (!message.content.replace(/<(.|\n)*?>/g, '').trim()) {
+        return false;
+    }
+    //
+    else if (message.content.includes("@here") || message.content.includes("@everyone")) return false;
 
-        else if (message.content.substring(0, 2) === "..") {
-            TextImageRedirect(message);
-        }
+    else if (message.content.substring(0, 2) === "..") {
+        TextImageRedirect(message);
+    }
 
-        // отправляем текстовое сообщение
-        else if (message.mentions.has(client.user.id)) {
-            TextImageRedirect(message);
-        }
-        //
-        else {
-            return;
-        }
-
-
-    });
-
-    client.on('interactionCreate', async interaction => {
-        if (interaction.isButton() && interaction.customId === "stable-dif") {
-            TextImageRedirect(interaction.message, interaction.message.content);
-
-            await interaction.reply({ content: "Картинка - " + interaction.message.content + " - обновляется. Подождите примерно 17 секунд!", ephemeral: true });
+    // отправляем текстовое сообщение
+    else if (message.mentions.has(client.user.id)) {
+        TextImageRedirect(message);
+    }
+    //
+    else {
+        return;
+    }
 
 
-        }
+});
+
+client.on('interactionCreate', async interaction => {
+    if (interaction.isButton() && interaction.customId === "stable-dif") {
+        TextImageRedirect(interaction.message, interaction.message.content);
+
+        await interaction.reply({ content: "Картинка - " + interaction.message.content + " - обновляется. Подождите примерно 17 секунд!", ephemeral: true });
 
 
-    })
+    }
+
+
+})
 
 keepAlive();
