d15c03a7490ef520bab2a8332c60ee2908c45733
vebbilder <66235658+vebbilder@users.noreply.github.com>
2022-11-20 14:43:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -43,24 +43,24 @@ exports.TextImageRedirect = function(param, param2) {
             let neuro = result.text;
 
             (async function() {
-                const massPromt = [
-                    'highly detailed, d & d, highly detailed, digital painting, trending on artstation, concept art, sharp focus, illustration, global illumination, ray tracing, realistic shaded, art by artgerm and greg rutkowski,super detailed picture, the smallest drawing of details, 4k, octane ,pastel halftones',
+                 const massPromt = [
+                    'highly detailed, d & d, highly detailed, digital painting, trending on artstation, concept art, sharp focus, illustration, global illumination, ray tracing, realistic shaded, super detailed picture, the smallest drawing of details, 4k, octane ,pastel halftones',
 
-                    'golden hour, awesome atmosphere, dean cornwell, norman rockwell, 8 k, octane rendered, sharp focus, highly detailed, volumetric lighting, illustration, concept art, paint texture, intricate, ruan jia, steve mccurry - n 9,super detailed picture, the smallest drawing of details, 4k, octane  ,pastel halftones',
+                    'golden hour, awesome atmosphere, 8 k, octane rendered, sharp focus, highly detailed, volumetric lighting, illustration, concept art, paint texture, intricate,super detailed picture, the smallest drawing of details, 4k, octane  ,pastel halftones',
 
-                    'andrei riabovitchev, marc simonetti, and sakimichan, trending on artstation,super detailed picture, the smallest drawing of details, 4k, octane ,pastel halftones',
+                    'trending on artstation,super detailed picture, the smallest drawing of details, 4k, octane ,pastel halftones',
 
                     'behance hd, artstation, deviantart, global illumination radiating a glowing aura global illumination ray tracing hdr render in unreal engine 5,super detailed picture, the smallest drawing of details, 4k, octane,pastel halftones',
 
-                    'digital painting, highly detailed, artstation, sharp focus, illustration, concept art, ruan jia, steve mccurry, amazing composition, fractal flame,super detailed picture, the smallest drawing of details, 4k, octane,pastel halftones',
+                    'digital painting, highly detailed, artstation, sharp focus, illustration, concept art,amazing composition, fractal flame,super detailed picture, the smallest drawing of details, 4k, octane,pastel halftones',
 
-                    'ultra-detailed, uhd 8k cryengine, octane render,super detailed picture, the smallest drawing of details, 4k, octane,highly detailed, digital painting, artstation, concept art, sharp focus, illustration, cinematic lighting, art by artgerm and greg rutkowski and alphonse mucha and simon stalenhag, high detail, artstation, octane render, 4 k resolution, masterpiece,pastel halftones',
+                    'ultra-detailed, uhd 8k cryengine, octane render,super detailed picture, the smallest drawing of details, 4k, highly detailed, digital painting, artstation, concept art, sharp focus, illustration, cinematic lighting, high detail, artstation, octane render, 4 k resolution, masterpiece,pastel halftones',
 
-                    'insanely detailed, by dan mumford, yusuke murata, makoto shinkai, ross tran, intricate detail, cinematic, 8 k, featured on artstation, pixiv,super detailed picture, the smallest drawing of details, 4k, octane ,realistic,pastel halftones',
+                    'insanely detailed,intricate detail, cinematic, 8 k, featured on artstation, pixiv,super detailed picture, the smallest drawing of details, 4k, octane ,realistic,pastel halftones',
 
-                    'detailed portrait, cell shaded, 4 k, concept art, ilya kuvshinov, artgerm, krenz cushart, greg rutkowski,cinematic dramatic atmosphere, sharp focus, volumetric lighting, cinematic lighting, studio quality,super detailed picture, the smallest drawing of details, 4k, octane, illustration,pastel halftones',
+                    'detailed portrait, cell shaded, 4 k, concept art, artgerm,cinematic dramatic atmosphere, sharp focus, volumetric lighting, cinematic lighting, studio quality,super detailed picture, the smallest drawing of details, 4k, octane, illustration,pastel halftones',
 
-                    'chilling eastern europen forrest. night, horroristic shadows, higher contrasts, (((lumnious))), theatrical, character concept art by ruan jia, (((thomas kinkade))), and j.dickenson, trending on pinterest, artstation,pastel halftones',
+                    'chilling eastern europen forrest. night, horroristic shadows, higher contrasts, (((lumnious))), theatrical, character concept art by ruan jia, and j.dickenson, trending on pinterest, artstation,pastel halftones',
 
                     'elegant, glowing lights, highly detailed, digital painting, artstation, concept art, smooth, sharp focus, illustration, alphonse muchas,super detailed picture,drawing of details, 4k, octanem,pastel halftones'
                 ];
