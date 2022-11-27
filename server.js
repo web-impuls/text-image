b73f158eb3f04ce667ae1985c3e8b4d4857cd7ec
vebbilder <66235658+vebbilder@users.noreply.github.com>
2022-11-27 06:17:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ exports.TextImageRedirect = async function(param, param2, param3) {
                 try {
 
                    await page.setUserAgent('5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
-                    page.setDefaultNavigationTimeout(0);
+                    //page.setDefaultNavigationTimeout(0);
                     await page.goto('https://www.mage.space/', { waitUntil: networkidle0 });
                     
                     await page.waitForTimeout('#search-bar');
